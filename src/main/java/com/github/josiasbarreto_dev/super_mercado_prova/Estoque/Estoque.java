417375f92e29857a7ceb9d163d11aaa31aa1d361
Josias Barreto <sr.josiasbarreto@gmail.com>
2025-05-30 12:03:34 -0300

    feat: implements stock class methods.

--- src/main/java/com/github/josiasbarreto_dev/super_mercado_prova/Estoque/Estoque.java
+++ src/main/java/com/github/josiasbarreto_dev/super_mercado_prova/Estoque/Estoque.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 
 public class Estoque {
     private int id = 1;
-    private ArrayList<Produto> listaDeProdutos;
+    private ArrayList<Produto> listaDeProdutos = new ArrayList<>();
 
     public Estoque(int id) {
         this.id = id;
@@ -29,45 +29,69 @@ public class Estoque {
     }
 
     public void inicializaEstoque(){
-     // Todo: Necessário implementar!
+        Produto produto1 = new Produto(1, "Caneta", 3.50, 10);
+        Produto produto2 = new Produto(2, "Lápis", 1.50, 5);
+        Produto produto3 = new Produto(3, "Borracha", 0.50, 100);
+        Produto produto4 = new Produto(4, "Caderno", 20.99, 15);
+
+         cadastraProduto(produto1);
+         cadastraProduto(produto2);
+         cadastraProduto(produto3);
+         cadastraProduto(produto4);
     }
 
     public Produto encontraProduto(String nome){
-        // Todo: Necessário implementar!
-        return new Produto(1, "Biscoito", 3.50, 10);
+       return getListaDeProdutos().stream()
+               .filter(p -> p.getNome().equalsIgnoreCase(nome))
+               .findFirst().orElse(null);
     }
 
     public Produto encontraProduto(int id){
-        // Todo: Necessário implementar!
-        return new Produto(1, "Biscoito", 3.50, 10);
+        return getListaDeProdutos().stream()
+                .filter(p -> p.getId() == id)
+                .findFirst().orElse(null);
     }
 
     public boolean cadastraProduto(Produto produto){
-        // Todo: Necessário implementar!
-        return true;
+        return getListaDeProdutos().add(produto);
     }
 
     public void imprimeCatalogoDoEstoque(){
-        System.out.println(" ");
+        for(Produto p : listaDeProdutos){
+            System.out.printf("ID: %d | Nome: %s | Preço: %.2f | Quantidade: %d\n",
+                    p.getId(), p.getNome(), p.getPreco(), p.getQuantidadeEmEstoque());
+        }
     }
 
     public boolean darBaixaEmEstoque(String nome, int quantidadeParaDarBaixa){
-        return true;
+        Produto p = encontraProduto(nome);
+        if(p != null || p.getQuantidadeEmEstoque() >= quantidadeParaDarBaixa){
+            int total = p.getQuantidadeEmEstoque() - quantidadeParaDarBaixa;
+            p.setQuantidadeEmEstoque(total);
+            return true;
+        }
+        return false;
     }
 
     public boolean darBaixaEmEstoque(int id, int quantidadeParaDarBaixa){
-        return true;
+       Produto p = encontraProduto(id);
+       if (p != null || p.getQuantidadeEmEstoque() >= quantidadeParaDarBaixa){
+           int total = p.getQuantidadeEmEstoque() - quantidadeParaDarBaixa;
+           p.setQuantidadeEmEstoque(total);
+           return true;
+       }
+        return false;
     }
 
     public int getQuantidadeAtualEmEstoque(Produto produto){
-        return 1;
+        return produto.getQuantidadeEmEstoque();
     }
 
     public int getPosicaoDoProdutoNaLista(Produto produto){
-        return 1;
+        return listaDeProdutos.indexOf(produto) + 1;
     }
 
     public boolean temEstoqueOuNao(Produto produto, int quantidadeParaDarBaixa){
-        return true;
+       return  getQuantidadeAtualEmEstoque(produto) >= quantidadeParaDarBaixa;
     }
 }
